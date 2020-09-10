70c4f7d0961487f55e94b9541bee35ef78582071
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2020-09-10 22:56:38 +0500

    Update Trains.Designer.cs

--- TrainReservationSystem/TrainReservationSystem/Trains.Designer.cs
+++ TrainReservationSystem/TrainReservationSystem/Trains.Designer.cs
@@ -5,6 +5,7 @@
         /// <summary>
         /// Required designer variable.
         /// </summary>
+        ///
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
